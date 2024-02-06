25cc99222e2fedf7950501a1711864073521925c
Gerardo Huerta Hernández <gerardohuerta1502@gmail.com>
2024-02-05 22:44:27 -0600

    Functions

---
+++ functions/src/main.rs
@@ -0,0 +1,21 @@
+// Statement are instructions that performs some actions and do not return a value
+// Expressions evaluate to a resultant value.
+
+fn main() {
+    let y = 6;
+    main1();
+}
+
+fn main1() {
+    println!("Hello, world!");
+    another_function(5);
+    print_labeled_measurement(5, 'h');
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
---
+++ functions/target/debug/deps/functions-4bba5a2f9f396a15.d
@@ -0,0 +1,9 @@
+/home/gerardohuerta1502/Escritorio/TheRustProgrammingLanguage/functions/target/debug/deps/libfunctions-4bba5a2f9f396a15.rmeta: src/main.rs Cargo.toml
+
+/home/gerardohuerta1502/Escritorio/TheRustProgrammingLanguage/functions/target/debug/deps/functions-4bba5a2f9f396a15.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
