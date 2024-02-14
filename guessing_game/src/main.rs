b51edec663b7f3c3169d3983b814dc51973d0b20
Gerardo Huerta Hernández <gerardohuerta1502@gmail.com>
2024-02-14 15:32:28 -0600

    Funciones

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,14 +6,13 @@ use std::io;
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is {secret_number}");
+    // println!("The secret number is {secret_number}");
     // Variable creation, we use the let statement to create a variable example:  let apples = 5;
     // Variables in rust are inmutables if we wanna make them mutables we add the word mut before the variable name
     // println!("You guessed: {guess}");
     loop {
         println!("Please input your guess: ");
         let mut guess = String::new();
-
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
