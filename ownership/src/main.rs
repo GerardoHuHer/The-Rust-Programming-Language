b72dcdc1551ae450b6c700fa489df40eddf03a4c
Gerardo Huerta Hernández <gerardohuerta1502@gmail.com>
2024-02-23 16:55:46 -0600

    Ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -3,4 +3,6 @@ fn main() {
     let mut w = String::from("Hello");
     w.push_str(", world!"); // push_str() appends a literal to a String
     println!("{}", w);
+    let s1 = String::from("Hello");
+    let s2 = s1;
 }
