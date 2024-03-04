d2f22dd5a36d4e326188963d434a8aa085b1dd3c
Gerardo Huerta Hernández <gerardohuerta1502@gmail.com>
2024-03-03 23:36:31 -0600

    References and Borrowing

--- referencesBorrowing/src/main.rs
+++ referencesBorrowing/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+    let mut s = String::from("Hello");
+    let len = calculate_length(&s1);
+    println!("The length of '{}' is {}.", s1, len);
+    println!("The message from s is '{}'", s);
+    change(&mut s);
+    println!("The new mesagge from s is '{}'", s)
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world!");
 }
