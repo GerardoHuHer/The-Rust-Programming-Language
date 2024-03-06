a5b58759feecfb554bbffd676750bff74e6a2f62
GerardoHuHer <gerardohuerta1502@gmail.com>
2024-03-06 17:36:38 -0600

    references

--- referencesBorrowing/src/main.rs
+++ referencesBorrowing/src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
     let s1 = String::from("hello");
-    let mut s = String::from("Hello");
     let len = calculate_length(&s1);
     println!("The length of '{}' is {}.", s1, len);
     println!("The message from s is '{}'", s);
