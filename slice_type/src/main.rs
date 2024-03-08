6f2faa4e5ec3af87c50cfce5b8ef8c5a54d3e10e
GerardoHuHer <gerardohuerta1502@gmail.com>
2024-03-07 21:17:13 -0600

    Slice_type

--- slice_type/src/main.rs
+++ slice_type/src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
     let s = String::from("Hello, world");
-    let num = first_word(&s);
-    println!("The length of the first string of '{}' is {}", s, num)
+    let num = first_word_len(&s);
+    let word = first_word(&s);
+    println!(
+        "The first string of '{}' is {}, and the length is {}",
+        s, word, num
+    )
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word_len(s: &String) -> usize {
     let bytes = s.as_bytes();
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
@@ -12,4 +16,14 @@ fn first_word(s: &String) -> usize {
         }
     }
     s.len()
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    &s[..]
 }
