d27fc63109e898c6f8b7231b064b3b5fe286ab76
GerardoHuHer <gerardohuerta1502@gmail.com>
2024-03-10 23:05:15 -0600

    Rectangles

---
+++ rectangles/src/main.rs
@@ -0,0 +1,38 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let width1 = 30;
+    let height1 = 50;
+    let rect1 = (30, 50);
+    println!(
+        "The area of the rectangle is {} square pixels, with the area function.",
+        area(width1, height1)
+    );
+    println!(
+        "The area of the rectangle is {} square pixels, with the area1 function.",
+        area1(rect1)
+    );
+    let rect2 = Rectangle {
+        width: 50,
+        height: 30,
+    };
+    println!(
+        "The area of the rectangle is {} square pixels, with the struct area function.",
+        area_struct(&rect2)
+    );
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn area1(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+fn area_struct(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
