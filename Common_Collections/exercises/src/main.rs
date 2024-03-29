790a27ec4a7d16d442830723de4907475cca69b3
Gerardo Huerta Hernández <gerardohuerta1502@gmail.com>
2024-03-29 01:45:51 -0600

    Exercises common collections

--- Common_Collections/exercises/src/main.rs
+++ Common_Collections/exercises/src/main.rs
@@ -1,12 +1,18 @@
 use std::collections::HashMap;
 
 fn main() {
+    let word = exercise2(&mut "Ale".to_string());
+    println!("{word}");
+}
+
+fn exercise1() {
+    println!("Exercise 1: Median and Mode");
     let mut arr: Vec<i32> = vec![1, 2, 3, 4, 5, 4, 5, 5, 6, 1, 1, 3, 2, 5, 5, 5, 5];
     let med = median(&mut arr);
-    println!("Median from {:?} is {}", arr, med);
-    mode(&arr)
-    // let mod1 = mode(&arr);
-    // println!("The mode of {:?} is {}", arr, mod1)
+    println!("The array is {:?}", arr);
+    println!("Median is {}", med);
+    let mod1 = mode(&arr);
+    println!("The mode is {}", mod1)
 }
 
 fn median(arr: &mut Vec<i32>) -> i32 {
@@ -22,8 +28,7 @@ fn median(arr: &mut Vec<i32>) -> i32 {
     }
 }
 
-fn mode(arr: &Vec<i32>) /* -> i32*/
-{
+fn mode(arr: &Vec<i32>) -> i32 {
     let mut map: HashMap<i32, i32> = HashMap::new();
     let mut ret_key: i32 = 0;
     let mut ret_value: i32 = 0;
@@ -32,11 +37,39 @@ fn mode(arr: &Vec<i32>) /* -> i32*/
         *count += 1;
     }
     for (key, value) in map {
-        println!("Number: {}, Occurs: {}", key, value)
-        // if ret_value < value {
-        //     ret_key = key;
-        //     continue;
-        // }
+        if ret_value < value {
+            ret_key = key;
+            ret_value = value;
+        }
     }
-    // return ret_key;
+    return ret_key;
+}
+
+fn is_vowel(c: char) -> bool {
+    match c {
+        'a' | 'e' | 'i' | 'o' | 'u' => true,
+        _ => false,
+    }
+}
+
+fn starts_with_vowel(word: &String) -> bool {
+    if let Some(first_char) = word.chars().next() {
+        is_vowel(first_char.to_ascii_lowercase())
+    } else {
+        false
+    }
+}
+fn exercise2(word: &mut String) -> String {
+    if (starts_with_vowel(&word)) {
+        word[..].to_string() + "-hay"
+    } else {
+        word[1..].to_string() + "-" + &word[..1] + "ay"
+    }
+}
+
+fn exercise3() {
+    /*
+    Using a hash map and vectors, create a text interface to allow a user to add employee names to a department in a company. For example, “Add Sally to Engineering” or “Add Amir to Sales.” Then let the user retrieve a list of all people in a department or all people in the company by department, sorted alphabetically.
+    */
+    let mut map: HashMap<String, Vec<String>> = HashMap::new();
 }
